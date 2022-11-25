e5ce4eef1fdf9121482efb60d1e400a22baf1963
Kristian Restrepo Osorio <103449864+kristianrpo@users.noreply.github.com>
2022-11-25 18:06:09 -0500

    Create scrapping_first_link.py

---
+++ PROYECTO 3/scrapping_first_link.py
@@ -0,0 +1,73 @@
+import requests
+import string
+from bs4 import BeautifulSoup
+import spacy
+def get_url_2 (url, diccionario,contador,posicion,cont2,cont3):
+    nlp = spacy.load("es_core_news_sm")
+    html_text = requests.get(url).text
+    soup = BeautifulSoup(html_text, 'html.parser')
+    todos_hiper = soup.find_all("a",href=True, title = True)
+    for i in range(len(todos_hiper)):
+        todos_hiper[i] = 'https://es.wikipedia.org' + todos_hiper[i]['href'], todos_hiper[i]['title']
+    for i in range(len(todos_hiper)):
+        doc = nlp(todos_hiper[i][1])
+        if todos_hiper[i][1].find("Anexo:")==-1:
+            if todos_hiper[i][1].find("Editar")==-1:
+                if todos_hiper[i][1].find("(aún no redactado)")==-1:
+                    if todos_hiper[i][1].find(" ")>=0:
+                        if todos_hiper[i][1].find("Categoría:")==-1:
+                            if todos_hiper[i][1].find("(")==-1:
+                                if todos_hiper[i][1].find(":")==-1:
+                                    if todos_hiper[i][1].find("Existe un")==-1:
+                                        for ent in doc.ents:
+                                            if ent.label_ == "PER":
+                                                if todos_hiper[i] not in diccionario:
+                                                    if cont2<1:
+                                                        diccionario[list(diccionario.keys())[0]].append(todos_hiper[i])
+                                                        diccionario[todos_hiper[i]] = []
+                                                        cont3+=1
+                                                    else:
+                                                        diccionario[list(diccionario.values())[posicion][contador]].append(todos_hiper[i])
+                                                        diccionario[todos_hiper[i]] = []
+                                                        cont3+=1
+                                                    
+                                        if cont3>0:
+                                            break
+
+
+
+def dfs(visited, graph, node,fin):
+    if node not in visited:
+        visited.append(node)
+        for neighbour in graph[node]:
+            if neighbour == fin:
+                visited.append(neighbour)
+                print("Si existe relacion entre {} y {}".format(nombreInicial,nombreBuscada))
+                break
+            else:
+                dfs(visited, graph, neighbour,fin)
+while True:
+    url = input("Ingrese url de wikipedia de la persona 1: ")
+    nombreInicial = input("Ingrese nombre de la persona inicial (tal cual escrito en wikipedia): ")
+    urlFin = input("Ingrese url de wikipedia de la persona 2: ")
+    nombreBuscada = input("Ingrese nombre de la persona buscada (tal cual escrito en wikipedia): ")
+    if url == '':
+        break 
+    else:
+        contador = -1
+        posicion = 0
+        cont3 = 0
+        diccionario = {(url, nombreInicial):[]}
+        get_url_2(url,diccionario,0,0,0,cont3)
+        cont3 = 0
+        for j in range(10):
+            for i in range(len(diccionario[list(diccionario.keys())[j]])):
+                contador+=1
+                get_url_2(list(diccionario.values())[j][0][0],diccionario,contador,j,2,cont3)
+            contador=-1
+        visited = []
+        dfs(visited,diccionario,(url, nombreInicial),(urlFin,nombreBuscada))
+        print("el camino fué: ")
+        for i in diccionario.keys():
+            print(i,"->", end = '')
+        break
